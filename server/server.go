a3320c95154f2fd227f7773e4ad8d921ed3d4578
allkamau <allangithaiga5@gmail.com>
2024-07-25 02:01:19 +0300

    Refactor server initialization for improved error handling and routing

---
+++ server/server.go
@@ -0,0 +1,23 @@
+package server
+
+import (
+	"ascii-art-web/handlers"
+	"log"
+	"net/http"
+)
+
+func RegisterHandlers() {
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	http.HandleFunc("/", handlers.ServeTemplate)
+	http.HandleFunc("/ascii-art", handlers.HandleAsciiArt)
+}
+
+func StartServer() {
+	RegisterHandlers()
+
+	log.Print("Listening on :http://localhost:8080/")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
